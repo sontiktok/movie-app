716770ac8d28d7ecbec11610f2ecb702118fb9dd
sontiktok <sonvltiktok@gmail.com>
2024-08-12 17:30:42 +0700

    Implements slider

--- src/components/FeatureMovies/PaginateIndicator.jsx
+++ src/components/FeatureMovies/PaginateIndicator.jsx
@@ -1,11 +1,14 @@
-const PaginateIndicator = () => {
+const PaginateIndicator = ({ movies, activeMovieId, setActiveMovieId }) => {
   return (
     <div className="absolute bottom-[10%] right-8">
       <ul className="flex gap-1">
-        <li className="h-1 w-6 cursor-pointer bg-slate-100"></li>
-        <li className="h-1 w-6 cursor-pointer bg-slate-600"></li>
-        <li className="h-1 w-6 cursor-pointer bg-slate-600"></li>
-        <li className="h-1 w-6 cursor-pointer bg-slate-600"></li>
+        {movies.map((movie) => (
+          <li
+            onClick={() => setActiveMovieId(movie.id)}
+            key={movie.id}
+            className={`h-1 w-6 cursor-pointer ${movie.id === activeMovieId ? "bg-slate-100" : "bg-slate-600"}`}
+          ></li>
+        ))}
       </ul>
     </div>
   );
--- src/components/FeatureMovies/index.jsx
+++ src/components/FeatureMovies/index.jsx
@@ -1,11 +1,38 @@
 import PaginateIndicator from "./PaginateIndicator";
 import Movie from "./Movie";
-
+import { useEffect, useState } from "react";
 const FeatureMovies = () => {
+  const [movies, setMovies] = useState([]);
+  const [activeMovieId, setActiveMovieId] = useState();
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/movie/popular", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjczYzE3YmVjOTBmMWU1Y2IzOWY4YWFlZjk3YWIzYSIsIm5iZiI6MTcyMzM2NzQ5NS4xNzg1MjcsInN1YiI6IjY2Yjg3ZTIxZTcyZjYwNDgxY2FhYjczOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kT63WUOpUR1z755ko5asofpqVG3SlWZmg3MqaQ0b7jM",
+      },
+    }).then(async (res) => {
+      const data = await res.json();
+      const popularMovies = data.results.slice(0, 4);
+      setMovies(popularMovies);
+      setActiveMovieId(popularMovies[0].id);
+    });
+  }, []);
+
   return (
     <div className="relative text-white">
-      <Movie />
-      <PaginateIndicator />
+      {movies
+        .filter((movie) => movie.id === activeMovieId)
+        .map((movie) => (
+          <Movie key={movie.id} data={movie} />
+        ))}
+      <PaginateIndicator
+        movies={movies}
+        activeMovieId={activeMovieId}
+        setActiveMovieId={setActiveMovieId}
+      />
     </div>
   );
 };
