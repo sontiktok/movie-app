40d9399ae884029bf5511f37ba300e9bc2d1b00e
sontiktok <sonvltiktok@gmail.com>
2024-08-13 00:46:44 +0700

    Refactor code, re-using MediaList Component

--- src/components/MediaList/index.jsx
+++ src/components/MediaList/index.jsx
@@ -1,47 +1,34 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
-const TABS = [
-  {
-    id: "all",
-    name: "All",
-  },
-  {
-    id: "tv",
-    name: "TV SHOW",
-  },
-  {
-    id: "movie",
-    name: "Movie",
-  },
-];
-const MediaList = () => {
+
+const MediaList = ({ title, tabs }) => {
   const [mediaList, setMediaList] = useState([]);
-  const [activeTabId, setActiveTabId] = useState(TABS[0].id);
+  const [activeTabId, setActiveTabId] = useState(tabs[0]?.id);
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/trending/${activeTabId}/day?language=en-US`,
-      {
+    const url = tabs.find((tab) => tab.id === activeTabId)?.url;
+    if (url) {
+      fetch(url, {
         method: "GET",
         headers: {
           accept: "application/json",
           Authorization:
             "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjczYzE3YmVjOTBmMWU1Y2IzOWY4YWFlZjk3YWIzYSIsIm5iZiI6MTcyMzM2NzQ5NS4xNzg1MjcsInN1YiI6IjY2Yjg3ZTIxZTcyZjYwNDgxY2FhYjczOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kT63WUOpUR1z755ko5asofpqVG3SlWZmg3MqaQ0b7jM",
         },
-      },
-    ).then(async (res) => {
-      const data = await res.json();
-      console.log({ data });
-      const trendingMediaList = data.results.slice(0, 12);
-      setMediaList(trendingMediaList);
-    });
-  }, [activeTabId]);
+      }).then(async (res) => {
+        const data = await res.json();
+        console.log({ data });
+        const trendingMediaList = data.results.slice(0, 12);
+        setMediaList(trendingMediaList);
+      });
+    }
+  }, [activeTabId, tabs]);
   console.log(mediaList);
   return (
     <div className="bg-black px-8 py-10 text-[1.2vw] text-white">
       <div className="mb-6 flex items-center gap-4">
-        <p className="text-[2vw] font-bold">Trending</p>
+        <p className="text-[2vw] font-bold">{title}</p>
         <ul className="flex rounded border border-white">
-          {TABS.map((tab) => (
+          {tabs.map((tab) => (
             <li
               key={tab.id}
               className={`cursor-pointer rounded px-2 py-1 ${tab.id === activeTabId ? "bg-white text-black" : ""}`}
@@ -50,11 +37,6 @@ const MediaList = () => {
               {tab.name}
             </li>
           ))}
-          {/* <li className="cursor-pointer rounded bg-white px-2 py-1 text-black">
-            All
-          </li>
-          <li className="cursor-pointer rounded px-2 py-1">Movie</li>
-          <li className="cursor-pointer rounded px-2 py-1">TV Show</li> */}
         </ul>
       </div>
 
@@ -66,7 +48,7 @@ const MediaList = () => {
             releaseDate={media.release_date || media.first_air_date}
             poster={media.poster_path}
             point={media.vote_average}
-            mediaType={media.media_type}
+            mediaType={media.media_type || activeTabId}
           />
         ))}
       </div>
---
+++ src/libs/constants.js
@@ -0,0 +1,30 @@
+export const TRENDING_TABS = [
+  {
+    id: "all",
+    name: "All",
+    url: "https://api.themoviedb.org/3/trending/all/day",
+  },
+  {
+    id: "tv",
+    name: "TV SHOW",
+    url: "https://api.themoviedb.org/3/trending/tv/day",
+  },
+  {
+    id: "movie",
+    name: "Movie",
+    url: "https://api.themoviedb.org/3/trending/movie/day",
+  },
+];
+
+export const TOP_RATED_TABS = [
+  {
+    id: "movie",
+    name: "Movie",
+    url: "https://api.themoviedb.org/3/movie/top_rated",
+  },
+  {
+    id: "tv",
+    name: "TV SHOW",
+    url: "https://api.themoviedb.org/3/tv/top_rated",
+  },
+];
